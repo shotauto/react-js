4289960d1034565836e401e0fb6350da1769284e
Uto Shota <64183202+shotauto@users.noreply.github.com>
2022-07-27 14:03:10 +0900

    after useState()

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
-  const contentStyle = {
-    color: "pink",
-    fontSize: "18px"
+  const onClickButton = () => {
+    setNum(num + 1);
   };
-
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは！</h1>
-      <ColorfulMessage color="blue" message="お元気ですか？" />
-      <p style={contentStyle}>元気です！</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
+      <ColorfulMessage color="pink">元気です!</ColorfulMessage>
+      <button onClick={onClickButton}>カウントアップ！</button>
+      <br />
+      <button>on/off</button>
+      <p>{num}</p>
+      <p>(><)</p>
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "18px"
   };
 
-  return <p style={contentStyle}>{props.message}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
